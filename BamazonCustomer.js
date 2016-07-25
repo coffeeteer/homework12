b81f314807747acc46877e9795a0accdf2cbfc01
Coffeeteer <tgrieshiemer@gmail.com>
2016-07-25 11:49:09 -0400

    fixed some issues with sql and node

---
+++ BamazonCustomer.js
@@ -0,0 +1,99 @@
+//NPM LIBRARIES
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var path = require('path');
+var itemNum;
+var itemId;
+var newAmount;
+
+//Syncs to MySQL
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root', //Username
+	password: '', //password
+	database: 'bamazon',
+	//method: 'GET'
+});
+
+//Creates connection with Server and makes table
+connection.connect(function(err) {
+	if(err) {
+		console.error('error with connection: ' + err.stack);
+	}
+	makeTable();
+});
+
+//Function that grabs the products table from database and prints results
+var makeTable = function() {
+	//Selects all of the data from the MySQL products table - SELECT COMMAND:
+	connection.query('SELECT * FROM products', function(err, res) {
+		if(err) throw err;
+		//prints the table to the consoe with minimal styling.
+		var tab = "\t";
+		console.log('ItemID\tProduct Name\tDepartment Name\tPrice\t# In Stock');
+		console.log('--------------------------------------------------------');
+		//FOR LOOP goes throguht the MySQL table and prints each individual row on a new line
+		for(var i = 0; i < res.length; i++) {
+			console.log(res[i].ItemID + tab + res[i].ProductName + tab + res[i].DepartmentName + tab + res[i].Price + tab + res[i].StockQuantity);
+		}
+		console.log('---------------------------------------------------------');
+		//runs the customers prompts after creating the table. Sends res so the promptCustomer function is able to search through the data
+		promptCustomer(res);
+	});
+}; 
+
+newAmount =function() {
+	connection.query();
+	console.log('Updating');
+};
+
+//function containing all customer prompts
+var promptCustomer = function() {
+	//prompts user for what they want to purchase
+	inquirer.prompt([{
+		type: 'input',
+		name: 'choice',
+		message: 'what would you like to purchase?'
+	}]).then(function(val) {
+		//set the VAR corrct to FALSE so as to amek sure the user inputs a valid product name
+		var correct = false;
+
+		//loops through the MySQL table to check that the product they wanted exists
+		for(var i = 0; i < res.length; i++) {
+			
+			//1. ToDo: if the product exists, set correct = true and ask the USER to see to many of the product they would like to buy
+			if(val.choice == res.productName) {
+				correct = true;
+				console.log(res[i].StockQuantity);
+				itemNum = res[i].StockQuantity;
+				itemId = res[i].ItemID;
+
+				inquirer.prompt([{
+					type: 'input',
+					name: 'choice2',
+					message: 'What would you like to buy?'
+				}]).then(function (val2) {
+					if(val2 <= itemNum) {
+						console.log('Great we have ' + itemNum);
+						buyInfo = itemNum - val2.choice2;
+
+
+					}
+				});
+
+			}
+			
+
+			//2. ToDo: Check to see if the amount requested is less than the amount that is available
+
+			//3. ToDo: Update the MySQL to reduce the StockQuantity by the amount requested - UPDATE command
+
+			//4. ToDo: Show the TABLE again by calling the cuntion that makes the table
+		}
+		//If the product requested does not exist, restarts PROMPT
+		if(i == res.length && correct == false){
+			promptCustomer(res);
+		}
+	});	
+};
